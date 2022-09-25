02ece9f41ea47cfed28883593bb63dc6491ab68a
Rendi Wijiatmoko <rendiwijiatmoko97@gmail.com>
2022-09-25 17:18:09 +0700

    change color active and border to blue

--- dwm/config.h
+++ dwm/config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel size of windows */
-static const unsigned int gappx     = 7;        /* gaps size between windows */
+static const unsigned int borderpx  = 3;        /* border pixel size of windows */
+static const unsigned int gappx     = 10;        /* gaps size between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -16,8 +16,8 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 //current tag and current window font color
 static const char col_gray4[]       = "#eeeeee";
-//Top bar second color (blue) and active window border color
-static const char col_cyan[]        = "#f59542";
+//Top bar second color and active window border color
+static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
